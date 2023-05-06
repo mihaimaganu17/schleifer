87a5eb694bc7f7274ec97a891197311bdcfdc015
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2023-05-06 14:30:08 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+
+/// Returns a type that implements the Responder trait
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
